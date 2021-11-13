0c594cd2162bc0cc3904f2369b482c441c559d23
jahnavi <kosalajahnavi656@gmail.com>
2021-11-13 09:26:44 +0530

    [ClassandObject]

---
+++ Classandobject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassandObject
+{
+     public class Calculator
+     {
+        int num1;
+        int num2;
+        int result;
+        void Add()
+        {
+            result = num1 + num2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        void substract()
+        {
+            result = num1 - num2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            Calculator obj = new Calculator();
+            obj.num1 = 10;
+            obj.num2 = 20;
+            obj.Add();
+            obj.substract();
+        }
+    }
+}
