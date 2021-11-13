4f044e7f914024f242c35550a03b0399b2324d52
jahnavi <kosalajahnavi656@gmail.com>
2021-11-13 09:16:41 +0530

    [Abstraction]

---
+++ AbstractBase.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPs
+{
+    internal abstract class AbstractBase
+    {
+        public void Add(int x, int Y)
+        {
+            Console.WriteLine(X + Y);
+        }
+        public override void Mul(int X, int Y);
+        {
+    }
+}
